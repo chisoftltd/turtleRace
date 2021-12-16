7799a2a7e986a851693687697e34a08badaa5b4b
chisoftltd <chisoftltd@gmail.com>
2021-12-16 16:41:39 +0000

    more commit

--- main.py
+++ main.py
@@ -6,14 +6,23 @@ screen = Screen()
 
 def move_forwards():
     crazy.forward(20)
-    crazy.setheading(45)
-    crazy.forward(20)
-    crazy.right(90)
-    crazy.forward(20)
-    crazy.left(135)
-    crazy.goto(-200, -100)
+
+
+def move_backwards():
+    crazy.forward(-20)
+
+
+def move_left():
+    crazy.left(20)
+
+
+def move_right():
+    crazy.right(40)
 
 
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=move_left)
+screen.onkey(key="d", fun=move_right)
 screen.exitonclick()
